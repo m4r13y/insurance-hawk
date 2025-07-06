9b5bd39e62be75944e1b2f6605708a21494e222c
Calvin Marley <calmarley@outlook.com>
2025-07-06 06:17:08 +0000

    Come up with a different logo/title style

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,13 +2,8 @@ import { cn } from '@/lib/utils';
 
 export function Logo({className}: {className?: string}) {
   return (
-    <div className={cn("flex items-center gap-2", className)}>
-      <h1 className="font-headline text-xl font-bold text-current">
-        Hawkins
-      </h1>
-      <div className="flex items-center justify-center h-9 w-9 rounded-full bg-primary text-primary-foreground">
-         <span className="text-xs font-bold font-headline">Nest</span>
-      </div>
-    </div>
+    <h1 className={cn("font-headline text-2xl font-bold", className)}>
+      Hawk<span className="text-accent font-semibold">Nest</span>
+    </h1>
   );
 }
