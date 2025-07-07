d3f6048ec9f2facb99743fa1d24de3b641326bdc
Calvin Marley <calmarley@outlook.com>
2025-07-07 02:02:27 +0000

    No it doesnt need a user-agent. Lets make sure it is properly getting my

--- src/app/dashboard/api-test/actions.ts
+++ src/app/dashboard/api-test/actions.ts
@@ -10,7 +10,6 @@ export async function testGetCounty(zipCode: string) {
 
     const apiGetHeaders = {
         'accept': 'application/json',
-        'User-Agent': 'HawkNestApp/1.0',
     };
     
     const url = `https://marketplace.api.healthcare.gov/api/v1/counties/by/zip/${zipCode}?apikey=${apiKey}`;
